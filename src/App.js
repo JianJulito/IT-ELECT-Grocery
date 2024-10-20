e00bf7d073549546f806db10878a6cecf0ed94f7
casanmacaan <macaancasan1@gmail.com>
2024-10-20 19:34:04 +0800

    final

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import GroceryList from './components/GroceryList';
+
+const App = () => {
+    return (
+        <div>
+            <GroceryList />s
+        </div>
+    );
+};
+
+export default App;
